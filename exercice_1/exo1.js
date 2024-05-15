d59c123d8d761c587052401075f961a67351f3b3
Maro Wagué <marowague@MacBook-Air-de-Maro.local>
2024-05-15 13:11:02 +0200

    reponse controle

---
+++ exercice_1/exo1.js
@@ -0,0 +1,10 @@
+
+//bus.push([10,0],[3,5],[5,8]);
+
+function retour (bus){
+   //for i in Range bus:
+   // utiliser reduce
+    return bus 
+}
+
+console.log(retour([10,0],[3,5],[5,8]));
---
+++ exercice_2/exo2.js
@@ -0,0 +1,14 @@
+//const carte = [];
+
+function  maskify (carte) {
+   compteur = 0;
+   for (let i=0;a; i++) {
+    i = "#"
+    carte = carte + i
+    return carte
+   }
+   };
+
+console.log(maskify(4556364607935616));
+
+
